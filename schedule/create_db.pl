3a907e4067e45bffcf62495cfa89d8e15fb3f622
Peter Kaagman <prjv.kaagman@gmail.com>
2023-10-01 17:03:31 +0200

    schedule not working

--- schedule/create_db.pl
+++ schedule/create_db.pl
@@ -1,2 +1,56 @@
 #! /usr/bin/env perl
-use feature ":5.10"
+use feature ":5.10";
+
+use DBI;
+use strict;
+
+my $driver = "SQLite";
+my $db = "../db/schedule.db";
+my $dsn = "DBI:$driver:dbname=$db";
+my $db_user = "";
+my $db_pass = "";
+my $dbh = DBI->connect($dsn,$db_user,$db_pass, { RaiseError => 1}) or die $DBI::errstr;
+
+my $qry = "Select rowid,* From color";
+my $sth = $dbh->prepare($qry) or die $dbh->errstr;
+$sth->execute() or die $sth->errstr;
+
+while (my $row = $sth->fetchrow_hashref()){
+  print "Row id: ",$row->{'rowid'};
+  print " Name: ",$row->{'name'};
+  print " Vallue: ",$row->{'vallue'},"\n";
+}
+print "\n";
+
+$qry = "Select rowid,* From circuit";
+$sth = $dbh->prepare($qry) or die $dbh->errstr;
+$sth->execute() or die $sth->errstr;
+
+while (my $row = $sth->fetchrow_hashref()){
+  print "Row id: ",$row->{'rowid'};
+  print " Name: ",$row->{'name'};
+  print " Color: ",$row->{'color'};
+  print " GPIO: ",$row->{'gpio'},"\n";
+}
+
+$dbh->do('Delete From schedule');
+$qry = "Insert Into schedule values(?,?,?,?)";
+$sth = $dbh->prepare($qry) or die $dbh->errstr;
+
+for (my $circuit = 1; $circuit <= 3; $circuit++){
+	for (my $day = 0; $day <= 6; $day++){
+		for (my $hour = 0; $hour <= 23; $hour++){
+			for (my $minute = 0; $minute < 60; $minute+=5){
+				printf("%d  => %d:%d:%d\n",$circuit,$day,$hour,$minute);
+				$sth->execute($circuit,
+					      $day,
+					      ($hour*3600+$minute*60),
+					      ($hour*3600+$minute*60+3*60)
+				) or die $sth->errstr;
+			}
+		}
+	}
+}
+
+$dbh->disconnect();
+
--- schedule/schedule.pl
+++ schedule/schedule.pl
@@ -1,28 +1,54 @@
 #! /usr/bin/env perl
 
 use feature ":5.10";
+use strict;
+use warnings;
 use RPi::WiringPi;
 use RPi::Const qw(:all);
+use Data::Dumper;
+
+use DBI;
+
+my $driver = "SQLite";
+my $db = "../db/schedule.db";
+my $dsn = "DBI:$driver:dbname=$db";
+my $db_user = "";
+my $db_pass = "";
+my $dbh = DBI->connect($dsn,$db_user,$db_pass, { RaiseError => 1}) or die $DBI::errstr;
 
 my $pi = RPi::WiringPi->new();
 
-say $pi->core_temp;
-say $pi->cpu_percent;
-say $pi->network_info;
-say $pi->pi_details;
-
-my $pin = $pi->pin(5);
-say "mode: ",$pin->mode;
-$pin->mode(OUTPUT);
-say "mode: ",$pin->mode;
-say "status: ",$pin->read;
-$pin->write(ON);
-say "status: ",$pin->read;
-$pin->write(OFF);
-say "status: ",$pin->read;
-
-my $gpio_17 = $pi->pin(0);
-my $gpio_27 = $pi->pin(2);
-my $gpio_22 = $pi->pin(3);
 
 
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+
+my $now = $hour*3600 + $min*60;
+my $qry = "Select ROWID,gpio,name From circuit";
+my $sth = $dbh->prepare($qry) or die $dbh->errstr;
+$sth->execute();
+while (my $row = $sth->fetchrow_hashref()){
+	# Initialize the pin
+	my $pin = $pi->pin($row->{'gpio'});
+	$pin->mode(OUTPUT);
+	# Should it be on or off?
+	my $qry2 =<<"END_QRY";
+Select count(circuit) as count 
+From schedule
+Where circuit = $row->{'rowid'}
+And day = $wday
+And $now Between start And end
+END_QRY
+	my $sth2 = $dbh->prepare($qry2) or die $dbh->errstsr;
+	$sth2->execute();
+	my $row2 = $sth2->fetchrow_hashref();
+	if ($row2->{'count'}){
+		say "GPIO: ", $row->{'name'}, " moet aan";
+		$pin->write(ON);
+	}else{
+		say "GPIO: ", $row->{'name'}, " moet uit";
+		$pin->write(OFF);
+	}
+	
+
+}
+$sth->finish();
