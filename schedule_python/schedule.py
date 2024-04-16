7ff5019984899e66368d3ec117dc4f5576538db6
Peter Kaagman <prjv.kaagman@gmail.com>
2024-04-16 09:04:07 +0200

    Python sqlachemy

--- schedule_python/schedule.py
+++ schedule_python/schedule.py
@@ -1,18 +1,8 @@
 #! /usr/bin/env python
 
-from typing import Optional
-from sqlmodel import Field, SQLModel, Session, create_engine, select
+from sqlalchemy import create_engine, text
 
-class Circuit(SQLModel, table=True):
-  name: str
-  color: str
-  gpio: int
-
-engine = create_engine("sqlite:///test.sqlite")
-
-SQLMOdel.metadata.create_all(engine)
-
-with Session(engine) as session:
-  statement = select(Circuit)
-  circuit = session.exec(statement).first()
-  print(circuit)
+engine = create_engine("sqlite+pysqlite:///../db/schedule.db", echo=True)
+with engine.connect() as conn:
+  result = conn.execute(text("select * from circuit"))
+  print(result.all())
